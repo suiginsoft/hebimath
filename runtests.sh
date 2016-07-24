8ee9605b20f390edd1343d84f5402bb82f36beb3
suigin <suigin@national.shitposting.agency>
2016-07-24 09:57:45 -0700

    make runtests.sh POSIX compliant

--- runtests.sh
+++ runtests.sh
@@ -2,14 +2,14 @@
 
 LD_LIBRARY_PATH=`pwd`
 
-echo "running $1 tests"
-echo "==============================================="
+printf "running %s tests\n" "$1"
+printf "=============================================\n"
 
 shift
-while [[ $# -gt 0 ]]; do
-	echo "$1" && $1;
+while [ $# -gt 0 ]; do
+	printf "%s\n" "$1" && $1;
 	shift
 done
 
-echo "==============================================="
-echo "all done"
+printf "=============================================\n"
+printf "all done\n"
