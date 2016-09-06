50f8871fb1c4c0b4f0eab0e8f960351be6b5656b
suigin <suigin@national.shitposting.agency>
2016-09-05 19:28:55 -0700

    added missing translation unit for hebi_psetu2 inline function

---
+++ src/p/psetu2.c
@@ -0,0 +1,10 @@
+/*
+ * hebimath - arbitrary precision arithmetic library
+ * See LICENSE file for copyright and license details
+ */
+
+#include "../../internal.h"
+
+extern HEBI_API
+void
+hebi_psetu2(hebi_packet *, uint64_t, uint64_t);
