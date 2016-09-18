4f0f8c6b8bc7090a9e59de40cb790ef08c895d37
suigin <suigin@national.shitposting.agency>
2016-09-18 03:00:41 -0700

    code cleanup, removed blank lines

--- src/p/generic/recipu2x1.c
+++ src/p/generic/recipu2x1.c
@@ -21,23 +21,17 @@ hebi_recipu64_2x1__(uint64_t d)
 	d8 = (d >> 55) & 0xFF;
 	d40 = (d >> 24) + 1;
 	d63 = (d >> 1) - d0;
-
 	v0 = hebi_recipu64_v0lut__[(size_t)d8];
 	v1 = (v0 << 11) - ((v0 * v0 * d40) >> 40) - 1;
 	v2 = (v1 << 13) + (((1152921504606846976ull - v1 * d40) * v1) >> 47);
-
 	e = ((v2 & d0) >> 1) - v2 * d63;
-
 	v3 = (v2 << 31) + ((uint64_t)((((hebi_uint128)v2) * e) >> 65));
-
 	f = (hebi_uint128)v3 * d;
 	f0 = (uint64_t)(f & UINT64_MAX);
 	f1 = (uint64_t)(f >> 64);
 	f0 = f0 + d;
 	f1 = f1 + d + (f0 < d);
-
 	v4 = v3 - f1;
-
 	return v4;
 }
 
@@ -54,15 +48,11 @@ hebi_recipu32_2x1__(uint32_t d)
 	d9 = (d >> 22) & 0x1FF;
 	d21 = (d >> 11) + 1;
 	d31 = (d >> 1) - d0;
-
 	v0 = hebi_recipu32_v0lut__[(size_t)d9];
 	v1 = (v0 << 4) - ((uint32_t)((((uint64_t)(v0 * v0)) * d21) >> 32)) - 1;
-
 	e = ((v1 & d0) >> 1) - v1 * d31;
-
 	v2 = (v1 << 15) + ((uint32_t)((((uint64_t)v1) * e) >> 33));
 	v3 = v2 - (((uint32_t)(((((uint64_t)v2) * d) + d) >> 32)) + d);
-
 	return v3;
 }
 
