185f4437b93e1d463bde18d05130c800e5f4dc01
suigin <suigin@national.shitposting.agency>
2016-09-27 23:45:56 -0700

    optimized pclz for mostly normalized packet arrays

--- src/p/x86_64/pclz.s
+++ src/p/x86_64/pclz.s
@@ -10,40 +10,35 @@
 .if HAS_HWCAP_AVX && HAS_HWCAP_LZCNT
 MVFUNC_BEGIN pclz, avx_lzcnt
 
-    mov         %rsi, %rcx
     shl         $5, %rsi
     vpcmpeqd    %xmm4, %xmm4, %xmm4
-    lea         -16(%rdi,%rsi), %rdi
+    mov         %rsi, %rcx
 
     .p2align 4
-1:  vmovdqa     (%rdi), %xmm0
-    vmovdqa     -16(%rdi), %xmm1
+1:  vmovdqa     -16(%rdi,%rsi), %xmm0
+    vmovdqa     -32(%rdi,%rsi), %xmm1
     vptest      %xmm4, %xmm0
-    jnz         2f
-    vptest      %xmm4, %xmm1
-    jnz         4f
-    sub         $32, %rdi
-    dec         %rcx
-    jnz         1b
-    lea         (,%rsi,8), %rax
-    ret
-
-    .p2align 4,,7
-2:  vpextrq     $1, %xmm0, %rax
-    shl         $5, %rcx
-    test        %rax, %rax
+    vpextrq     $1, %xmm0, %rax
     jnz         3f
-    vmovq       %xmm0, %rax
-    add         $8, %rsi
-3:  lzcnt       %rax, %rax
-    sub         %rcx, %rsi
-    lea         (%rax,%rsi,8), %rax
+    vptest      %xmm4, %xmm1
+    vpextrq     $1, %xmm1, %rax
+    jnz         2f
+    sub         $32, %rsi
+    jnz         1b
+    lea         (,%rcx,8), %rax
     ret
 
     .p2align 4,,7
-4:  add         $16, %rsi
-    vmovdqa     %xmm1, %xmm0
-    jmp         2b
+2:  vmovdqa     %xmm1, %xmm0
+    sub         $16, %rsi
+3:  test        %rax, %rax
+    jnz         4f
+    vmovq       %xmm0, %rax
+    sub         $8, %rsi
+4:  lzcnt       %rax, %rax
+    sub         %rsi, %rcx
+    lea         (%rax,%rcx,8), %rax
+    ret
 
 MVFUNC_END
 .endif
@@ -53,41 +48,36 @@ MVFUNC_END
 .if HAS_HWCAP_SSE41
 MVFUNC_BEGIN pclz, sse41
 
-    mov         %rsi, %rcx
     shl         $5, %rsi
     pcmpeqd     %xmm4, %xmm4
-    lea         -16(%rdi,%rsi), %rdi
+    mov         %rsi, %rcx
 
     .p2align 4
-1:  movdqa      (%rdi), %xmm0
-    movdqa      -16(%rdi), %xmm1
+1:  movdqa      -16(%rdi,%rsi), %xmm0
+    movdqa      -32(%rdi,%rsi), %xmm1
     ptest       %xmm4, %xmm0
-    jnz         2f
-    ptest       %xmm4, %xmm1
-    jnz         4f
-    sub         $32, %rdi
-    dec         %rcx
-    jnz         1b
-    lea         (,%rsi,8), %rax
-    ret
-
-    .p2align 4,,7
-2:  pextrq      $1, %xmm0, %rax
-    shl         $5, %rcx
-    test        %rax, %rax
+    pextrq      $1, %xmm0, %rax
     jnz         3f
-    movq        %xmm0, %rax
-    add         $8, %rsi
-3:  bsr         %rax, %rax
-    sub         %rcx, %rsi
-    xor         $63, %rax
-    lea         (%rax,%rsi,8), %rax
+    ptest       %xmm4, %xmm1
+    pextrq      $1, %xmm1, %rax
+    jnz         2f
+    sub         $32, %rsi
+    jnz         1b
+    lea         (,%rcx,8), %rax
     ret
 
     .p2align 4,,7
-4:  add         $16, %rsi
-    movdqa      %xmm1, %xmm0
-    jmp         2b
+2:  movdqa      %xmm1, %xmm0
+    sub         $16, %rsi
+3:  test        %rax, %rax
+    jnz         4f
+    movq        %xmm0, %rax
+    sub         $8, %rsi
+4:  bsr         %rax, %rax
+    sub         %rsi, %rcx
+    xor         $63, %rax
+    lea         (%rax,%rcx,8), %rax
+    ret
 
 MVFUNC_END
 .endif
@@ -112,9 +102,9 @@ MVFUNC_BEGIN pclz, sse2
     pmovmskb    %xmm2, %eax
     pmovmskb    %xmm3, %edx
     cmp         $0xFFFF, %eax
-    jne         2f
+    jne         3f
     cmp         $0xFFFF, %edx
-    jne         4f
+    jne         2f
     sub         $32, %rdi
     dec         %rcx
     jnz         1b
@@ -122,23 +112,20 @@ MVFUNC_BEGIN pclz, sse2
     ret
 
     .p2align 4,,7
-2:  shl         $5, %rcx
+2:  add         $16, %rsi
+    mov         %edx, %eax
+    movdqa      %xmm1, %xmm0
+3:  shl         $5, %rcx
     cmp         $0xFF, %ah
-    je          3f
+    je          4f
     punpckhqdq  %xmm0, %xmm0
     sub         $8, %rsi
-3:  movq        %xmm0, %rax
+4:  movq        %xmm0, %rax
     bsr         %rax, %rax
     sub         %rcx, %rsi
     xor         $63, %rax
     lea         64(%rax,%rsi,8), %rax
     ret
-
-    .p2align 4,,7
-4:  add         $16, %rsi
-    mov         %edx, %eax
-    movdqa      %xmm1, %xmm0
-    jmp         2b
 
 MVFUNC_END
 .endif
