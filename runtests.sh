a30fec6c55ad1d36f460136f11ac98169307eebc
suigin <suigin@national.shitposting.agency>
2016-12-29 21:53:27 -0800

    assign and export LD_LIBRARY_PATH separately to avoid masking return value

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=$(pwd)
+LD_LIBRARY_PATH=$(pwd)
+export LD_LIBRARY_PATH
 
 printf "running %s tests\n" "$1"
 printf "=============================================\n"
