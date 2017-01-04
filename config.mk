fcbc317ec81f2950eb286e8ba580e0a2802b516d
suigin <suigin@national.shitposting.agency>
2017-01-04 07:59:40 -0800

    enabled SonarQube scanner for Travis CI

--- config.mk
+++ config.mk
@@ -40,7 +40,7 @@ AS = as
 
 # toolchain flags
 CFLAGS = -O3 -std=c99 -pedantic -pthread -Wall -Wextra -Waggregate-return \
-	 -Wconversion -Wmissing-prototypes -Wshadow
+	 -Wconversion -Wshadow
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_POSIX_C_SOURCE=200809L
 ASFLAGS =
 LDFLAGS = -s
