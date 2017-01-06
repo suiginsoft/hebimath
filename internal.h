7d1b413463eb44d632172898c68b92058bbb11f3
suigin <suigin@national.shitposting.agency>
2017-01-06 01:25:55 -0800

    added allocid field to hebi_kiss struct

--- internal.h
+++ internal.h
@@ -284,10 +284,11 @@ hebi_error_assert__(const char *expr, const char *func, const char *file, long l
 #define hk_xorshift hebi_xorshift__
 #define hk_congruential hebi_congruential__
 #define hk_carry hebi_carry__
-#define hk_index hebi_index__
-#define hk_length hebi_length__
 #define hk_single hebi_single__
 #define hk_multi hebi_multi__
+#define hk_length hebi_length__
+#define hk_index hebi_index__
+#define hk_allocid hebi_allocator__
 
 /* hebi_integer field names */
 #define hz_packs hebi_packs__
